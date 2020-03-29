58026633c322dfb0b1752cc019f66ebd5cd7d0f2
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-03-29 16:39:00 +0200

    An overview of the work with ./main.py

--- ok/src/main.py
+++ ok/src/main.py
@@ -16,12 +16,16 @@ def main():
     shader = Shader(shader_dir + "color.vert", shader_dir + "color.frag")
     terrain_shader = Shader(shader_dir + "terrain.vert", shader_dir + "terrain.frag")
 
-    translate_keys = {0: vec(0, 0, 0), 2: vec(1, 1, 0), 4: vec(0, 0, 0)}
-    rotate_keys = {0: quaternion(), 2: quaternion_from_euler(180, 45, 90),
-                   3: quaternion_from_euler(180, 0, 180), 4: quaternion()}
-    scale_keys = {0: 1, 2: 0.5, 4: 1}
+    translate_keys = {5: vec(0, 0, 0), 8: vec(1, 1, 0), 15: vec(0, 0, 0)}
+    rotate_keys = {5: quaternion(), 8: quaternion_from_euler(180, 45, 90),
+                   10: quaternion_from_euler(180, 0, 180), 15: quaternion()}
+    scale_keys = {5: 1, 8: 0.5, 15: 1}
 
     light_dir = vec(-1, 0, 1)
+
+    # floor
+    terrain = Terrain(tests_dir + "img/brown.png", tests_dir + "img/height.jpg", terrain_shader, light_dir=light_dir, k_d=(0.3, 0.3, 0))
+    viewer.add(terrain)
 
     # lapin pas éclairé
     rabbit = Node(transform=translate(0, 1, 0))
@@ -29,13 +33,16 @@ def main():
     rabbit.add(*[mesh for mesh in load(tests_dir + "bunny/bunny.obj", shader)])
     rabbit_rotation.add(rabbit)
 
-    cube = Node(transform=translate(0, 1.5, 0) @ rotate((0, 1, 0), 45))
+    cube = Node(transform=rotate((0, 1, 0), 45))
     cube_rotation = RotationControlNode(glfw.KEY_UP, glfw.KEY_DOWN, (1, 0, 0))
     cube.add(rabbit_rotation, *[mesh for mesh in load(tests_dir + "cube/cube.obj", shader, light_dir)])
     cube_rotation.add(cube)
 
+    cube_translation = Node(transform=translate(0, 1.5, 0))
+    cube_translation.add(cube_rotation)
+
     keynode = KeyFrameControlNode(translate_keys, rotate_keys, scale_keys)
-    keynode.add(cube_rotation)
+    keynode.add(cube_translation)
 
     # chargement d'un objet sans texture
     suzanne = Node(transform=translate(-2.5, 0, 0))
@@ -48,9 +55,6 @@ def main():
     viewer.add(suzanne_uncolored)
 
     viewer.add(keynode)
-
-    terrain = Terrain(tests_dir + "img/sand.png", tests_dir + "img/height.jpg", terrain_shader, light_dir=light_dir, k_d=(0.3, 0.3, 0))
-    viewer.add(terrain)
 
     # start rendering loop
     viewer.run()
