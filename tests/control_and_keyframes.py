3cb6842155669e28dfa6cad9fb11f8c5d4f4d5c5
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-04-02 09:15:09 +0200

    Control and keyframes tests done

--- tests/control_and_keyframes.py
+++ tests/control_and_keyframes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Test children adding
+Test keyboard control and keyframe animation
 """
 
 import sys
@@ -20,19 +20,31 @@ def main():
     # Shader
     color_shader = scene.shaders['color']
 
-    # TODO
     # Parameters
     rotation_matrix = rotate((0, 1, 0), 45) @ rotate((1, 0, 0), 45)
-    anim = {"rotation_control": True,
-            "key_up": glfw.KEY_UP,
-            "key_down": glfw.KEY_DOWN,
-            "axis": (1, 0, 0)
-           }
+    x_rotation = {"rotation_control": True,
+                  "key_up": glfw.KEY_DOWN,
+                  "key_down": glfw.KEY_UP,
+                  "axis": (1, 0, 0)
+                 }
+    y_rotation = {"rotation_control": True,
+                  "key_up": glfw.KEY_RIGHT,
+                  "key_down": glfw.KEY_LEFT,
+                  "axis": (0, 1, 0)
+                 }
     keyframe_anim = {"keyframes": True,
-                     "translate_keys": {5: vec(0, 0, 0), 8: vec(1, 1, 0), 15: vec(0, 0, 0)},
-                     "rotate_keys": {5: quaternion(), 8: quaternion_from_euler(180, 45, 90), 10: quaternion_from_euler(180, 0, 180), 15: quaternion()},
-                     "scale_keys": {5: 1, 8: 0.5, 15: 1}
+                     "translate_keys": {1: vec(0, 0, 0), 4: vec(1, 1, 0), 9: vec(0, 0, 0)},
+                     "rotate_keys": {1: quaternion(), 4: quaternion_from_euler(180, 45, 90), 7: quaternion_from_euler(180, 0, 180), 9: quaternion()},
+                     "scale_keys": {1: 1, 4: 0.4, 9: 1}
                     }
+
+    # Objects creation
+    granit_cube = Object(color_shader, "granit_cube", "obj/cube/cube.obj", tex_file="img/granit.jpg")
+    cube = Object(color_shader, "cube", "obj/cube/cube.obj", position=(0, 0.85, 0), scaling=(0.7, 0.7, 0.7))
+
+    # Adding children
+    granit_cube.add(cube, keyframes=keyframe_anim, rotation_control=y_rotation)
+    scene.add(granit_cube, keyframes=keyframe_anim, rotation_control=x_rotation)
 
     scene.viewer.run()
 
