7716e54155fdb3719e8fa5ee0b9a9e9a874abbc2
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-04-06 15:59:54 +0200

    Using inheritance for surfaces

--- src/objects.py
+++ src/objects.py
@@ -193,12 +193,62 @@ class Cylinder(Node):
         self.add(*load('cylinder.obj', shader))  # just load cylinder from file
 
 
-class TerrainAttributes:
-    def __init__(self, texture_map, height_map, max_color, max_height, size):
+class Surface(Mesh):
+    """ Generic surface """
+    def __init__(self, texture_map, max_height = 10, size = 50, light_dir=(0, 1, 0),
+                 k_a=(0, 0, 0), k_d=(1, 1, 0), k_s=(0.1, 0.1, 0.1), s=16):
+        self.light_dir = light_dir
+        self.k_a, self.k_d, self.k_s, self.s = k_a, k_d, k_s, s
+
+        # interactive toggles
+        self.wrap = cycle([GL.GL_MIRRORED_REPEAT, GL.GL_REPEAT,
+                           GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
+        self.filter = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
+                             (GL.GL_LINEAR, GL.GL_LINEAR),
+                             (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
+        self.wrap_mode, self.filter_mode = next(self.wrap), next(self.filter)
+        self.texture_map = texture_map
+        # setup texture and upload it to GPU
+        # Peut-être aussi vérifier pour names
+        self.texture = Texture(texture_map, self.wrap_mode, *self.filter_mode)
+
+    def key_handler(self, key):
+        # some interactive elements
+        if key == glfw.KEY_F6:
+            self.wrap_mode = next(self.wrap)
+            self.texture = Texture(self.texture_map, self.wrap_mode, *self.filter_mode)
+        if key == glfw.KEY_F7:
+            self.filter_mode = next(self.filter)
+            self.texture = Texture(self.texture_map, self.wrap_mode, *self.filter_mode)
+
+    def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
+        """ vérifier pour diffuse_map ? """
+        GL.glUseProgram(self.shader.glid)
+
+        # texture access setups
+        GL.glActiveTexture(GL.GL_TEXTURE0)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.texture.glid)
+        GL.glUniform1i(self.loc['diffuse_map'], 0)
+
+        # setup light parameters
+        GL.glUniform3fv(self.loc['light_dir'], 1, self.light_dir)
+
+        # setup material parameters
+        GL.glUniform3fv(self.loc['k_a'], 1, self.k_a)
+        GL.glUniform3fv(self.loc['k_d'], 1, self.k_d)
+        GL.glUniform3fv(self.loc['k_s'], 1, self.k_s)
+        GL.glUniform1f(self.loc['s'], max(self.s, 0.001))
+
+        # world camera position for Phong illumination specular component
+        w_camera_position = np.linalg.inv(view)[:,3]
+        GL.glUniform3fv(self.loc['w_camera_position'], 1, w_camera_position)
+
+        super().draw(projection, view, model, primitives)
+
+
+class Attributes:
+    def __init__(self, texture_map, max_color, max_height, size):
         self.texture_map = Image.open(texture_map).convert('RGBA')
-        height = Image.open(height_map).convert('L')
-        # crop to a square height_map
-        self.height_map = height.crop((0, 0, min(height.size), min(height.size)))
         self.max_color = max_color
         self.max_height = max_height
         self.size = size
@@ -231,6 +281,14 @@ class TerrainAttributes:
         height_down = self.get_height(x, z+1)
         normal = vec(height_left - height_right, 2, height_up - height_down)
         return normal / np.linalg.norm(normal)
+
+
+class TerrainAttributes(Attributes):
+    def __init__(self, texture_map, height_map, max_color, max_height, size):
+        super().__init__(texture_map, max_color, max_height, size)
+        height = Image.open(height_map).convert('L')
+        # crop to a square height_map
+        self.height_map = height.crop((0, 0, min(height.size), min(height.size)))
 
     def generate_attributes(self):
         """
@@ -281,7 +339,14 @@ class TerrainAttributes:
         return vertices, normals, indices
 
 
-class Terrain(Mesh):
+class WaterAttributes(Attributes):
+    # TODO
+    # La texture utilisée peut être une image de couleur uniforme pour l'eau
+    def __init__(self):
+        pass
+
+
+class Terrain(Surface):
     """ Simple first textured object """
     # Ajouter assertion si fichiers non trouvés
     def __init__(self, texture_map, height_map, shader, max_color = 256, max_height = 10, size = 50,
@@ -289,57 +354,19 @@ class Terrain(Mesh):
         self.attrib = TerrainAttributes(texture_map, height_map, max_color, max_height, size)
         self.vertices, self.normals, self.indices = self.attrib.generate_attributes()
 
-        super().__init__(shader, [self.vertices, self.normals], self.indices)
-        self.light_dir = light_dir
-        self.k_a, self.k_d, self.k_s, self.s = k_a, k_d, k_s, s
+        super().__init__(texture_map, max_height, size, light_dir, k_a, k_d, k_s, s)
+        Mesh.__init__(self, shader=shader, attributes=[self.vertices, self.normals], index=self.indices)
 
         names = ['diffuse_map', 'light_dir', 'k_a', 's', 'k_s', 'k_d', 'w_camera_position']
-        # names = ['light_dir', 'k_a', 's', 'k_s', 'k_d', 'w_camera_position']
         loc = {n: GL.glGetUniformLocation(shader.glid, n) for n in names}
         self.loc.update(loc)
 
-        # interactive toggles
-        self.wrap = cycle([GL.GL_MIRRORED_REPEAT, GL.GL_REPEAT,
-                           GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
-        self.filter = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
-                             (GL.GL_LINEAR, GL.GL_LINEAR),
-                             (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
-        self.wrap_mode, self.filter_mode = next(self.wrap), next(self.filter)
-        self.texture_map = texture_map
-        # setup texture and upload it to GPU
-        self.texture = Texture(texture_map, self.wrap_mode, *self.filter_mode)
         print('Loaded terrain \t(x=[%s, %s], z=[%s, %s], %s faces)' % (-self.attrib.size/2, self.attrib.size/2,
                                                                        -self.attrib.size/2, self.attrib.size/2,
                                                                        (self.attrib.height_map.size[0]**2) * 2))
 
-    def key_handler(self, key):
-        # some interactive elements
-        if key == glfw.KEY_F6:
-            self.wrap_mode = next(self.wrap)
-            self.texture = Texture(self.texture_map, self.wrap_mode, *self.filter_mode)
-        if key == glfw.KEY_F7:
-            self.filter_mode = next(self.filter)
-            self.texture = Texture(self.texture_map, self.wrap_mode, *self.filter_mode)
 
-    def draw(self, projection, view, model, primitives=GL.GL_TRIANGLES):
-        GL.glUseProgram(self.shader.glid)
-
-        # texture access setups
-        GL.glActiveTexture(GL.GL_TEXTURE0)
-        GL.glBindTexture(GL.GL_TEXTURE_2D, self.texture.glid)
-        GL.glUniform1i(self.loc['diffuse_map'], 0)
-
-        # setup light parameters
-        GL.glUniform3fv(self.loc['light_dir'], 1, self.light_dir)
-
-        # setup material parameters
-        GL.glUniform3fv(self.loc['k_a'], 1, self.k_a)
-        GL.glUniform3fv(self.loc['k_d'], 1, self.k_d)
-        GL.glUniform3fv(self.loc['k_s'], 1, self.k_s)
-        GL.glUniform1f(self.loc['s'], max(self.s, 0.001))
-
-        # world camera position for Phong illumination specular component
-        w_camera_position = np.linalg.inv(view)[:,3]
-        GL.glUniform3fv(self.loc['w_camera_position'], 1, w_camera_position)
-
-        super().draw(projection, view, model, primitives)
+class Water(Surface):
+    # TODO
+    def __init__(self):
+        pass
