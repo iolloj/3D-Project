ec05ba20663eeba8f7bd631d9acf87c7d2784723
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-04-16 21:08:09 +0200

    Edges problems fixed

--- src/objects.py
+++ src/objects.py
@@ -598,6 +598,9 @@ class Skybox(Mesh):
         for index, texture in enumerate(self.textures):
             # Right, Left, Top, Bottom, Back, Front faces
             GL.glTexImage2D(GL.GL_TEXTURE_CUBE_MAP_POSITIVE_X+index, 0, GL.GL_RGBA, 512, 512, 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, texture)
+        GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP_TO_EDGE)
+        GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_R, GL.GL_CLAMP_TO_EDGE)
+        GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP_TO_EDGE)
         GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR)
         GL.glTexParameteri(GL.GL_TEXTURE_CUBE_MAP, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR)
 
