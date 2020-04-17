f23ddafdacfbf9d2f72883eb3e91d39b9e8b9f80
Jacopo Iollo <jacopo.iollo@grenoble-inp.com>
2020-04-17 12:05:23 +0200

    change gitignore

--- src/main.py
+++ src/main.py
@@ -11,8 +11,12 @@ def main():
 
     color_shader = scene.shaders['color']
 
-    scene.generate_terrain("../others/img/sand.jpg", "../others/img/height.jpg", 10, 100)
+    #scene.generate_terrain("../others/img/sand.jpg", "../others/img/height.jpg", 10, 100)
     scene.generate_water("../others/img/blu.jpg", 100)
+    # Boids
+    boids = Boids(color_shader, 27, "../obj/Fish/LionFish/LionFish.obj", scaling=0.1, index=0)
+    
+    #scene.add(boids)
 
     scene.viewer.run()
 
