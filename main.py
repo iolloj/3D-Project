658ad48fa69437eff8dd68e9b42670e76ae20b8e
bellecar <carole.bellet@yahoo.fr>
2020-04-24 17:54:23 +0200

    added bird to main scene

--- main.py
+++ main.py
@@ -17,23 +17,19 @@ def main():
 
     boids = Boids(color_shader, 27, "obj/Fish/BlueTang/BlueTang.obj", scaling=0.3, index=0)
     boids_placement = {
-        "position": (-10, -10, 200)
+        "position": (-5, -15, 200)
     }
     scene.add(boids, place_boids=boids_placement)
 
     rotation_matrix = rotate((0, 1, 0), 45) @ rotate((1, 0, 0), 45)
     
-    anim = {"rotation_control": True, "key_up": glfw.KEY_UP, "key_down": glfw.KEY_DOWN, "axis": (3, 0, 3)}
+    anim = {"rotation_control": True, "key_up": glfw.KEY_UP, "key_down": glfw.KEY_DOWN, "axis": (1, 1, 1)}
     keyframe_anim = {
         "keyframes": True,
         "translate_keys": {5: vec(0, 0, 0), 8: vec(1, 1, 0), 15: vec(0, 0, 0)},
         "rotate_keys": {5: quaternion(), 8: quaternion_from_euler(180, 45, 90), 10: quaternion_from_euler(180, 0, 180), 15: quaternion()},
         "scale_keys": {5: 1, 8: 0.5, 15: 1}
     }
-    
-    #Adding a bird TODO le faire voler autour d'une/des colonne(s) & trouver un oiseau
-    flying_bird = Object(color_shader, "Flying Brid", "obj/Fish/NurseShark/NurseShark.obj", position=(50, 30, 0), scaling=(1, 1, 1), tex_file = "obj/Fish/NurseShark/NurseShark_Base_Color.png")
-    scene.add(flying_bird, rotation_control=anim, keyframes=keyframe_anim)
     
     #Loading and adding Hercules to the scene TODO Tourver la bonne rotation
     Hercules = Object(color_shader, "Hercules", "obj/others/hercules/Hercules.obj", position=(10, -20, 150), scaling=(0.7, 0.7, 0.7 ), rotation_axis=(1, 0, 0), rotation_angle=-90, tex_file="obj/others/hercules/Hercules.jpg")
@@ -42,8 +38,15 @@ def main():
     #Loading the two columns, ionic and ionic_2
     column = Object(color_shader, "column", "obj/others/ionic/ionic.obj", position=(50, 20, 0), scaling=(0.2, 0.2, 0.2), rotation_axis=(1, 0, 0), rotation_angle=-100)
     column_2 = Object(color_shader, "column_2", "obj/others/ionic/ionic.obj", position=(55, 25, 10), scaling=(0.2, 0.2, 0.2), rotation_axis=(1, 0, 0), rotation_angle=-100)
+    
     #Adding the columns to the scene
     scene.add(column, column_2)
+    
+    
+    #Adding a bird TODO le faire voler autour d'une/des colonne(s) & trouver un oiseau
+    flying_bird = Object(color_shader, "Flying Brid", "obj/others/bird/base.fbx", position=(15, 30, -10), scaling=(1, 1, 1), tex_file = "obj/others/bird/body_baseColor.png")
+    scene.add(flying_bird, rotation_control=anim) #, keyframes=keyframe_anim)
+    
     
     #scene.viewer.add(("seahorse_skinned", *[m for m in load_skinned("obj/Fish/SeaHorse/SeaHorse.fbx", skinning_shader, tex_file="obj/Fish/SeaHorse/SeaHorse_Base_Color.png")]))#, position = (15, -10, 250))
     #seahorse_skinned.set_position(position=(-10, -5, 0), scaling=(0.15, 0.15, 0.15))
